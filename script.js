7da8288b83b3d020ab8158471608b722b08842d7
AlexGrin1 <a_grynevich@mail.ru>
2021-01-15 00:09:01 +0300

    refactor: changed pull-out menu

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 
+const link = document.querySelector('.burger_nav');
 const menu =  document.querySelector(".burger");
 const nav = document.querySelector('.burger_menu_none');
 menu.addEventListener('click', () => {
      nav.classList.toggle("burger_menu_show");
      menu.classList.toggle("burger_active");
-     
   });
+link.addEventListener('click', () => {
+   nav.classList.remove("burger_menu_show");
+   menu.classList.remove("burger_active");
+});
