f3066b9881c67275adc02ccf108c8a070ab6c579
AlexGrin1 <a_grynevich@mail.ru>
2021-01-23 23:23:35 +0300

    refactor: added darkening of the page when the menu is open

--- script.js
+++ script.js
@@ -28,14 +28,32 @@ filter.addEventListener('click', (event) => {
 const link = document.querySelector('.burger_nav');
 const burgMenu =  document.querySelector(".burger");
 const nav = document.querySelector('.burger_menu_none');
+const aside = document.querySelector('aside');
+const body = document.querySelector('body');
 
 burgMenu.addEventListener('click', () => {
      nav.classList.toggle("burger_menu_show");
+     nav.classList.toggle("burger_menu_none");
      burgMenu.classList.toggle("burger_active");
+     aside.classList.toggle('aside-overlay');
+     body.classList.add('no_scroll');
+     
+   });
+aside.addEventListener('click', (event) => {
+   if (event.target === aside) {
+       aside.classList.remove('aside-overlay');
+       burgMenu.classList.remove("burger_active");
+       nav.classList.remove("burger_menu_show");
+       nav.classList.add("burger_menu_none");
+       body.classList.remove('no_scroll');
+      }
   });
 link.addEventListener('click', () => {
    nav.classList.remove("burger_menu_show");
    burgMenu.classList.remove("burger_active");
+   aside.classList.remove('aside-overlay')
+   nav.classList.add("burger_menu_none");
+   body.classList.remove('no_scroll');
 });
 
 // переключение слайдов
